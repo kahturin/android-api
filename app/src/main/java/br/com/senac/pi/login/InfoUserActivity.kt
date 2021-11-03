60142a5e65bc32c8ef1c1bec9254a6a58dee5834
Djalma.Oliveira <djalma.oliveira@intecomlogistica.com.br>
2021-11-02 21:17:59 -0300

    :rocket: Inicio Info user"

---
+++ app/src/main/java/br/com/senac/pi/login/InfoUserActivity.kt
@@ -0,0 +1,12 @@
+package br.com.senac.pi.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.senac.pi.R
+
+class InfoUserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_info_user)
+    }
+}
